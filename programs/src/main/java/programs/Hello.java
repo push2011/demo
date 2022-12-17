7fb678971503bfa4775e342712ce27458da7da03
Admin <Admin@KSV_PUSHPENDRA>
2022-12-17 15:06:19 +0530

    Changes in hello world program

--- programs/src/main/java/programs/Hello.java
+++ programs/src/main/java/programs/Hello.java
@@ -2,7 +2,7 @@ package programs;
 
 public class Hello {
 	public static void main(String[] args) {
-
-		System.out.println("Hello World :::: !");
+		String s = "Hello World :::: !";
+		System.out.println("Message :::: " + s);
 	}
 }
